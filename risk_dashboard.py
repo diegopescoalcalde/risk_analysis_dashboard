e0bbec1cfe3c6486c040bbb2617f90590c744c89
Diego Pesco Alcalde <dp.alcalde@gmail.com>
2023-11-21 14:22:12 -0300

    first commit

---
+++ risk_dashboard.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objects as go
+from scipy.stats import shapiro, norm
+import risk_functions
+
+
+
+def home():
+    st.title('Home')
+    st.header('Welcome! Please Read me!')
+    st.markdown('''
+            Thank you for accessing this simple risk analysis dashboard. Below you can find
+            some useful information about the concepts explored in this app.
+            In the sidebar menu you can access the main sections of the dashboard:''')
+    with st.expander('* Ticker Info'):
+        st.markdown('''
+                    This section has charts that provide an overall picture of the ticker prices and returns
+                    in the past. 
+                * Close Price Chart 
+                * Returns Line Chart
+                * Returns Histogram 
+                * Shapiro-Wilk Normality Test \n
+                    This statistical test calculates the confidence to which the returns distribution can be
+                    approximated to a normal distribution. Usually a p-value lower than 0.05 is enough to the
+                    assumption of normality. This is a very useful information since normal distribution
+                    have many properties that can be used in risk assessment.
+                    ''')
+        
+    with st.expander('* Model Backtest and Comparison'):
+        st.markdown('''
+                This section has charts that will help you backtest and visualize the estimated 
+                VaR using different models and parameters. It is a helpful tool to understand the
+                effect of each model and parameter in the overall risk assessment. The available models are:
+                    
+                * Historical VaR \n
+                    This method calculates VaR from the a percentile of the past return 
+                    distribution, assuming that the future return distribution will be similar to the historical one.
+                    The two parameters to be defined are the confidence level to be applied and the rolling window from 
+                    which the past return distribution will be built. 
+            
+                * EWMA VaR \n
+                    This method also calculates VaR from past returns, but it calculates a 
+                    Exponentially Weighted Moving Average, where the earlier values have more weight
+                    than older values, in an attempt to appropriately model market heteroscedasticity.
+                    The parameters to be defined are the confidence level to be applied and the decay factor
+                    lambda. Lower lambda values will put more weight into earlier values and higher lambda will
+                    distribute weights more evenly.
+
+                * GARCH VaR \n
+                    The GARCH model is a more complex approach that takes into account the conditional
+                    autoregressive heteroscedasticity of the volatility. In other words, the estimated
+                    volatility is a function of the volatility and the residuals of the previous time steps.
+                    The parameters to be defined are the confidence level and the number of lag terms (p,q) in the
+                    function, but extensive research has proved that the best estimations are obtained in single
+                    lag model, i.e. GARCH(1,1).
+                
+            ''')
+                
+    with st.expander('Anomaly Detection'):
+        st.markdown('''
+                Soon..
+                    ''')
+                
+                
+def ticker_info():
+    st.title('Ticker Info')
+
+    dict_tickers = {
+        'S&P500':'^GSPC',
+        'NASDAQ':'^IXIC'
+    }
+
+    tickers_options = ['S&P500', 'NASDAQ']
+
+    tickers = st.selectbox('Choose Ticker', tickers_options)
+
+    st.markdown('Select area with mouse to zoom charts, click twice to zoom out.')
+
+    yf_data = yf.download(dict_tickers[tickers], period='5y', interval='1d')
+
+    fig = go.Figure(data=[go.Candlestick(x=yf_data.index,
+                                         open=yf_data['Open'],
+                                         high=yf_data['High'],
+                                         low=yf_data['Low'],
+                                         close=yf_data['Close'],
+                                         increasing_line_color= 'green', 
+                                         decreasing_line_color= 'red')])
+    fig.update_layout(xaxis_title='Date',
+                      yaxis_title='Price',
+                      title=tickers, 
+                      xaxis_rangeslider_visible=False)
+    st.plotly_chart(fig)
+
+    returns, fig_close_prices, fig_returns_line, fig_returns_hist, normality_test = risk_functions.compute_returns(tickers, pd.DataFrame(yf_data['Close']))
+    #st.plotly_chart(fig_close_prices)
+    st.plotly_chart(fig_returns_line)
+    st.plotly_chart(fig_returns_hist)
+    st.text('Shapiro-Wilk Normality Test Results')
+    st.text(f'Test Statistic:{round(normality_test[0], 5)}')
+    st.text(f'P-value:{round(normality_test[1], 5)}')
+
+def model_comparison():
+    st.title('Model Comparison')
+
+    dict_tickers = {
+        'S&P500':'^GSPC',
+        'NASDAQ':'^IXIC'
+    }
+
+    tickers_options = ['S&P500', 'NASDAQ']
+
+    tickers = st.selectbox('Choose Ticker', tickers_options)
+
+    yf_data = yf.download(dict_tickers[tickers], period='5y', interval='1d')
+    returns, fig_close_prices, fig_returns_line, fig_returns_hist, normality_test = risk_functions.compute_returns(tickers, pd.DataFrame(yf_data['Close']))
+    models = st.multiselect('Select Risk Models', ['Historical VaR', 'EWMA VaR', 'GARCH VaR'], ['EWMA VaR'])
+    var_list = []
+    error_list = []
+    confidence_level = st.number_input('Confidence Level', min_value=0.00, max_value=1.00, value=0.95)
+    if 'EWMA VaR' in models:
+        lambda_value = st.number_input('Lambda Value', min_value=0.00, max_value=1.00, value=0.94)
+        returns = risk_functions.compute_ewma_var(returns, lambda_value=lambda_value, time_horizon=1, confidence_level=confidence_level)
+        var_list.extend(['EWMA VaR', 'EWMA VaR - Negative Returns'])
+        error_list.extend(['EWMA VaR - Total Percentage Error', 'EWMA VaR - 365D Rolling Percentage Error'])
+    if 'Historical VaR' in models:
+       window = st.number_input('Window Value in Days', min_value=1, max_value=2000, value=1095)
+       returns = risk_functions.calculate_historical_var(returns, window=window, confidence_level=confidence_level)
+       var_list.extend(['Historical VaR', 'Historical VaR - Negative Returns'])
+       error_list.extend(['Historical VaR - Total Percentage Error', 'Historical VaR - 365D Rolling Percentage Error'])
+    if 'GARCH VaR' in models:
+        model_results, garch_var, returns = risk_functions.compute_garch_var(returns, p=1, q=1, confidence_level=confidence_level)
+        #st.write(model_results.summary())
+        var_list.extend(['GARCH VaR', 'GARCH VaR - Negative Returns'])
+        error_list.extend(['GARCH VaR - Total Percentage Error', 'GARCH VaR - 365D Rolling Percentage Error'])
+        #st.write(returns)
+
+    returns, fig_percentage_error, fig_go = risk_functions.plot_var(returns, var_list=var_list, error_list=error_list)
+    st.plotly_chart(fig_go)
+    st.plotly_chart(fig_percentage_error)
+
+        
+
+
+
+def main():
+    st.sidebar.title('Risk Analysis Dashboard')
+    st.sidebar.markdown('---')
+    menu_list=['Home', 'Ticker Info', 'Model Comparison']
+    choice = st.sidebar.radio('Window', menu_list)
+
+    if choice=='Home':
+        home()
+    if choice=='Ticker Info':
+        ticker_info()
+    if choice=='Model Comparison':
+        model_comparison()
+
+
+main()
---
+++ risk_functions.py
@@ -0,0 +1,184 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objects as go
+from scipy.stats import shapiro, norm
+import arch
+
+
+def compute_returns(tickers, close_prices):
+  returns = close_prices.pct_change().dropna()
+  returns.columns = ['Returns']
+
+  # Calculate normal distribution with same mean and std as the returns
+  returns['Normal Distribution'] = np.random.normal(loc=returns['Returns'].mean(), scale=returns['Returns'].std(), size=len(returns))
+
+  # Line plot close prices
+  fig_close_prices = px.line(close_prices,
+            x=close_prices.index,
+            y=['Close'],
+            color_discrete_sequence=px.colors.qualitative.G10)
+
+  fig_close_prices.update_layout(title=f"{tickers} Close Price",
+      xaxis_title="Date",
+      yaxis_title="Close Price",
+      legend=dict(title='Legend',
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+
+
+  # Line plot returns
+  fig_returns_line = px.line(returns,
+            x=returns.index,
+            y=['Returns'],
+            color_discrete_sequence=px.colors.qualitative.G10)
+
+  fig_returns_line.update_layout(title=f"{tickers} Daily Returns",
+      xaxis_title="Date",
+      yaxis_title="Returns",
+      legend=dict(title='Legend',
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+
+  # Histogram of returns
+  fig_returns_hist = px.histogram(returns,
+            x=['Returns', 'Normal Distribution'],
+                    barmode='overlay',
+                    color_discrete_sequence=px.colors.qualitative.G10)
+
+  fig_returns_hist.update_layout(title=f"{tickers} Returns Distribution",
+      xaxis_title="Returns",
+      legend=dict(title='Legend',
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+  
+  # Shapiro-Wilk normality test
+  normality_test = shapiro(returns)
+
+
+  return returns, fig_close_prices, fig_returns_line, fig_returns_hist, normality_test
+
+def calculate_historical_var(df_returns, window, confidence_level, min_periods=1):
+  risk_percentile = (1-confidence_level)/2
+  df_returns['Historical VaR'] = df_returns['Returns'].rolling(window=window, min_periods=min_periods).quantile(1-risk_percentile).shift(1)
+  df_returns['Historical VaR - Negative Returns'] = df_returns['Returns'].rolling(window=window, min_periods=min_periods).quantile(risk_percentile).shift(1)
+  # Calculate percentage error
+  df_returns['Actual > Predicted'] = (df_returns['Returns'] > df_returns['Historical VaR']) | (df_returns['Returns'] < df_returns['Historical VaR - Negative Returns'])
+  df_returns['Historical VaR - Total Percentage Error'] = df_returns['Actual > Predicted'].expanding().sum()*100 / df_returns['Actual > Predicted'].expanding().count()
+  df_returns['Historical VaR - 365D Rolling Percentage Error'] = df_returns['Actual > Predicted'].rolling('365D').sum()*100 / df_returns['Actual > Predicted'].rolling('365D').count()
+  
+  return df_returns
+
+def compute_ewma_var(returns, lambda_value, time_horizon, confidence_level):
+  risk_percentile = 1-((1-confidence_level)/2)
+
+  # Calculate squared returns
+  returns['Squared Returns'] = returns['Returns']**2
+
+  # Calculate EWMA volatility
+  returns['EWMA Volatility'] = pd.Series(returns['Squared Returns']).ewm(alpha=1-lambda_value, adjust=False).mean().pow(0.5).shift(1)
+  returns['Negative EWMA Volatility'] = -returns['EWMA Volatility']
+
+  # Calculate the critical value for the standard normal distribution
+  z_score_critical_value = norm.ppf(risk_percentile)
+
+  # Calculate VaR
+  returns['EWMA VaR'] = returns['EWMA Volatility'] * z_score_critical_value * np.sqrt(time_horizon)
+  returns['EWMA VaR - Negative Returns'] = -returns['EWMA VaR']
+
+  # Calculate percentage error
+  returns['Actual > Predicted'] = returns['Returns'].abs() > returns['EWMA VaR']
+  returns['EWMA VaR - Total Percentage Error'] = returns['Actual > Predicted'].expanding().sum()*100 / returns['Actual > Predicted'].expanding().count()
+  returns['EWMA VaR - 365D Rolling Percentage Error'] = returns['Actual > Predicted'].rolling('365D').sum()*100 / returns['Actual > Predicted'].rolling('365D').count()
+
+  return returns
+
+def compute_garch_var(returns, p, q, confidence_level):
+   # Fit model
+   model = arch.arch_model(returns['Returns'], mean='Constant', vol='GARCH', p=p, q=q)
+   model_results = model.fit()
+   
+   # Calculate VaR
+   risk_percentile = 1-((1-confidence_level)/2)
+   percentile = model.distribution.ppf([risk_percentile])
+   forecast = model_results.forecast(start=returns.index[0])
+   cond_mean = forecast.mean[:]
+   cond_variance = forecast.variance[:]
+   garch_var =  cond_mean.values + np.sqrt(cond_variance).values * percentile
+   garch_var = pd.DataFrame(garch_var, index=returns.index)
+   returns['GARCH VaR'] = garch_var.shift(1)
+   returns['GARCH VaR - Negative Returns'] = -garch_var.shift(1)
+
+   # Calculate percentage error
+   returns['Actual > Predicted'] = returns['Returns'].abs() > returns['GARCH VaR']
+   returns['GARCH VaR - Total Percentage Error'] = returns['Actual > Predicted'].expanding().sum()*100 / returns['Actual > Predicted'].expanding().count()
+   returns['GARCH VaR - 365D Rolling Percentage Error'] = returns['Actual > Predicted'].rolling('365D').sum()*100 / returns['Actual > Predicted'].rolling('365D').count()
+
+
+   return model_results, garch_var, returns
+
+
+def plot_var(returns, var_list, error_list):
+  
+  colors=px.colors.qualitative.G10
+
+  fig_go = go.Figure()
+  fig_go.add_trace(go.Scatter(
+            x=returns.index,
+            y=returns['Returns'],
+            name='Returns',
+            line=dict(color=colors[0])
+                ))
+  for i, var in enumerate(var_list):
+    if var in returns.columns:
+        fig_go.add_trace(go.Scatter(
+                    x=returns.index,
+                    y=returns[var],
+                    name=var,
+                    line=dict(color=colors[i+1])
+                        ))
+  
+  fig_go.update_layout(title="Predicted VaR x Actual Returns",
+      xaxis_title="Date",
+      yaxis_title="Returns",
+      legend=dict(title='Legend',
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+  
+  
+  fig_percentage_error = go.Figure()
+  for i, error_metric in enumerate(error_list):
+    if error_metric in returns.columns:
+        fig_percentage_error.add_trace(go.Scatter(
+                    x=returns.index,
+                    y=returns[error_metric],
+                    name=error_metric,
+                    line=dict(color=colors[i])
+                        ))
+  
+  fig_percentage_error.update_layout(title="Error Percentage",
+      xaxis_title="Date",
+      yaxis_title="Error Percentage (%)",
+      legend=dict(title='Legend',
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+
+
+  return returns, fig_percentage_error, fig_go
